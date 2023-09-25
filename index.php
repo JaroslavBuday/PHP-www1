248519ce81aeb3142d283083e5306b33643ab3a2
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-09-25 14:11:47 +0200

    premenna a html tag

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ echo "<br>";
 $school = "Bradavice"; //premenna
 echo($school);  // vypis iba premennej
 echo "<br>";
-echo("Škola: $school") // vypis premenna a text
+echo("Škola: $school"); // vypis premenna a text
 
 // pravidla premennej
 // velke a male pismena
@@ -29,6 +29,14 @@ echo("Škola: $school") // vypis premenna a text
 // viacslovne oddelovat podtrznikom napr school_name takzv. snake_case
 // pomlcky nesmu oddelovat
 
+//premenna a html
+$school_name = "Bradavice";
+echo "<h1>$school_name</h1>";
+$sub_heading = "škola čar a kúzel";
+echo "<h2>$sub_heading</h2>";
+
+$paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, officia.";
+echo "<p>$paragraph</p>";
 ?>
 
 
