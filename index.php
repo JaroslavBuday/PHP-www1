3d49fc3659c61ab40fc7ef433a2cf30de57ed70e
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-09-25 14:16:39 +0200

    zakomentovanie php vsetko

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <?php
 // jednoriadkovy komentar v php
 # prikaz echo a vypis do stranky:
- echo "Prikaz echo vypise do stranky text";
-echo "<br>";
-echo "vypisuje aj html kody";
-echo "<br>";
-echo 666;
-echo "<br>";
+//  echo "Prikaz echo vypise do stranky text";
+// echo "<br>";
+// echo "vypisuje aj html kody";
+// echo "<br>";
+// echo 666;
+// echo "<br>";
 
-var_dump("prikaz vardump");
-echo "<br>";
-var_dump(100);
-echo "<br>";
-var_dump(5.8);
-echo "<br>";
+// var_dump("prikaz vardump");
+// echo "<br>";
+// var_dump(100);
+// echo "<br>";
+// var_dump(5.8);
+// echo "<br>";
+// 
 /*
  viacriadkovy komentar 
 */
 
-$school = "Bradavice"; //premenna
-echo($school);  // vypis iba premennej
-echo "<br>";
-echo("Škola: $school"); // vypis premenna a text
+// $school = "Bradavice"; //premenna
+// echo($school);  // vypis iba premennej
+// echo "<br>";
+// echo("Škola: $school"); // vypis premenna a text
 
 // pravidla premennej
 // velke a male pismena
@@ -30,13 +31,15 @@ echo("Škola: $school"); // vypis premenna a text
 // pomlcky nesmu oddelovat
 
 //premenna a html
-$school_name = "Bradavice";
-echo "<h1>$school_name</h1>";
-$sub_heading = "škola čar a kúzel";
-echo "<h2>$sub_heading</h2>";
+// $school_name = "Bradavice";
+// echo "<h1>$school_name</h1>";
+// $sub_heading = "škola čar a kúzel";
+// echo "<h2>$sub_heading</h2>";
 
-$paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, officia.";
-echo "<p>$paragraph</p>";
+// $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, officia.";
+// echo "<p>$paragraph</p>";
+
+
 ?>
 
 
