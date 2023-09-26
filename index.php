2038a18594710d6e39131ffaf97e5900b26c2d11
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-09-26 08:57:30 +0200

    matematicke operacie v php

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
+// matematicke operacie
 
+$students_2022 = 100;
+$students_2023 = 89;
 
+$result1 = $students_2022 - $students_2023;
+$result2 = $students_2022 + $students_2023;
+$result3 = $students_2022 * $students_2023;
+$result4 = $students_2022 / $students_2023;
 
+echo "Rozdiel: $result1 ";
+echo "<br>";
+echo "Súčet: $result2 ";
+echo "<br>";
+echo "Súčin: $result3 ";
+echo "<br>";
+echo "Deleno: $result4 ";
+echo "<br>";
 ?>
 
 
