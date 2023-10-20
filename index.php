b09c786cb47ff4bbbe26e35e58bb56d01ccaff28
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-10-20 09:24:40 +0200

    Koniec opakovania html

--- index.php
+++ index.php
@@ -1,72 +1,13 @@
-<?php
-// prepojenie s HTML
-
-// video
-?>
-
-
 <!DOCTYPE html>
-<html lang="sk">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Training PHP sites</title>
+    <title>Document</title>
 </head>
 <body>
-    <header>
-        
-    </header>
-    <main>
-        <section>
-            <h1>Formular</h1>
-            <form>
-                <label for="FirstName">Meno</label>
-                <input type="text" name="first_name" id="FirstName"><br>
-                <label for="Weight">Váha</label>
-                <input type="number" name="weight" id="Weight" min="10" max="300"><br>
-                <input type="url" value="https://www." ><br>
-                <input type="text" name="second_name" placeholder="Priezvisko" autofocus required><br> 
-                <input type="password" name="password" placeholder="Heslo" required><br>
-                <input type="email" name="email" placeholder="E-mail"><br>
-                <input type="date" name="date"><br>
-                <input type="hidden" name="form-type" value="kontakt">
-                <textarea name="message" placeholder="Vaša správa" readonly></textarea><br>
-                
-    <!-- readonly - iba na citanie bez moznosti pisnia - pozri textarea,
-        autofocus- po refresnuti stranky bude kurzor v danom policku(priezvisko), 
-        disabled- vypnute (zalozka), 
-        required- nutne vyplnit (e-kniha) -->
-
-                <input type="checkbox" value="tricko">Tričko za 1 Eur <br>
-                <input type="checkbox" value="zalozka" disabled>Založka do knihy zdarma <br>
-                <input type="checkbox" value="E-kniha" required>E-kniha <b>Ako zbohatnúť</b> za 5 Eur <br><br>
-                
-                <select name="color">
-                    <optgroup label="Matná farba">
-                        <option value="red">červená</option>
-                        <option value="green">Zelená</option>
-                        <option value="yellow" selected>Žltá</option>
-                        <!-- selected vyberie co ma byt ako nahladova moznost -->
-                    </optgroup>
-                    <optgroup label="Lesklá farba">
-                        <option value="red2">červená</option>
-                        <option value="green2">Zelená</option>
-                        <option value="yellow2">Žltá</option>
-                    </optgroup>
-                </select><br>
-
-                <input type="radio" name="country" value="SR">Slovensko<br>
-                <input type="radio" name="country" value="AU">Rakúsko<br>
-                <input type="radio" name="country" value="DE">Nemecko<br>
-                <!-- ak je name rovnaka hodnota da sa vybrat iba jedna moznost -->
-                <button>Odoslať</button>
-
-            </form>
-        </section>
-        
-    </main>
-    <footer>
-
-    </footer>
+    <h1>Hlavná stránka</h1>
+    <a href="contact.php">Formulár</a><br>
+    <a href="cennik.php">Cenník</a>
 </body>
 </html>
