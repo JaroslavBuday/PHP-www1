1a13d62454f0fcc7937006406ef5deb93f2c9cf1
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-10-20 09:38:46 +0200

    cennik.php prepojenie HTML a PHP priklad

---
+++ cennik.php
@@ -0,0 +1,22 @@
+<?php
+    $price = 320
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cenník našich služieb</title>
+</head>
+<body>
+    <h1>Cenník</h1>
+    <p>Cena našich služieb je <?= $price;   ?> Eur.</p>
+    <!-- (php echo) je to iste ako dat = za znacku  -->
+</body>
+</html>
